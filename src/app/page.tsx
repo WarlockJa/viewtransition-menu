b0fe5125ffe7433d486cf97fd59c09cd96e6458c
warlockja <warlockja@gmail.com>
2023-11-11 20:35:25 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,142 @@
-import Image from 'next/image'
+"use client";
+import "./globals.css";
+import { nav } from "./component/links";
+import { useCallback, useRef, useState } from "react";
+import { flushSync } from "react-dom";
 
-export default function Home() {
+function Home() {
+  const [activeSub, _setActiveSub] = useState<null | number>(null);
+  const isTranstioningRef = useRef(false);
+  const prevSubRef = useRef<null | number>(null);
+
+  const updateNavigation = (id: number | null) => {
+    if (prevSubRef.current && id) {
+      document.documentElement.style.setProperty(
+        "--subnav-direction",
+        prevSubRef.current < id ? "1" : "-1"
+      );
+    }
+    _setActiveSub(id);
+  };
+
+  const setActiveSub = useCallback(
+    async (id: number | null) => {
+      if (isTranstioningRef.current || id === activeSub) return;
+
+      isTranstioningRef.current = true;
+      if (document.startViewTransition) {
+        const transition = document.startViewTransition(() => {
+          flushSync(() => {
+            updateNavigation(id);
+          });
+        });
+        await transition.finished;
+      } else {
+        updateNavigation(id);
+      }
+
+      isTranstioningRef.current = false;
+      prevSubRef.current = id;
+    },
+    [activeSub]
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <div className="bg-slate-200">
+      <div
+        role="figure"
+        className="absolute left-0 right-0 top-0 h-[400px] w-full bg-gradient-to-tr from-blue-700 to-blue-900 [clip-path:polygon(0_100%,100%_0,0_0)]"
+      >
+        <div className="absolute -left-24 top-0 h-[400px] w-[80vw] rounded-full bg-red-400 blur-3xl" />
+        <div className="absolute right-0 top-0 h-64 w-[40vw] rounded-full bg-orange-400 blur-[100px]" />
+      </div>
+      <main className="relative z-10 min-h-screen px-12 pt-36">
+        <header className="fixed left-12 right-12 top-0 flex items-baseline gap-4 py-4 text-white">
+          <p className="text-2xl">
+            <strong>MEGA</strong>menu
+          </p>
+          <nav onPointerLeave={() => setActiveSub(null)}>
+            <ul className="group flex">
+              {nav.map((item) => (
+                <li
+                  key={item.id}
+                  className="relative"
+                  onPointerEnter={() => setActiveSub(item.id)}
+                >
+                  {!item.subnavigation && (
+                    <a href={item.title} className="peer block px-2 py-2">
+                      {item.title}
+                    </a>
+                  )}
+                  {item.subnavigation && (
+                    <>
+                      <button
+                        className="peer block px-2 py-2"
+                        onFocus={() => setActiveSub(item.id)}
+                        onClick={() => setActiveSub(item.id)}
+                        aria-expanded={activeSub === item.id}
+                        aria-controls={`subnav-${item.id}`}
+                      >
+                        {item.title}
+                      </button>
+                      <div
+                        id={`subnav-${item.id}`}
+                        className="absolute left-0 top-full hidden w-[500px] rounded-lg bg-white p-1 text-black [view-transition-name:subnav] peer-aria-expanded:block"
+                      >
+                        <div className="absolute -top-2 left-8 h-0 w-0 border-b-[12px] border-l-[12px] border-r-[12px] border-b-white border-l-transparent border-r-transparent" />
+                        <div className="flex [view-transition-name:subnavcontent]">
+                          {item.leftBar && (
+                            <div className="min-h-[300px] w-[140px] rounded-sm bg-gray-100 px-4 py-5">
+                              <p className="text-sm">{item.leftBar}</p>
+                            </div>
+                          )}
+                          <div className="w-full">
+                            <ul className="grid grid-cols-2 gap-2 p-4">
+                              {item.subnavigation.map((subitem) => (
+                                <li key={subitem.title}>
+                                  <a href={subitem.href}>{subitem.title}</a>
+                                </li>
+                              ))}
+                            </ul>
+                            {item.bottomBar && (
+                              <div className="mt-4 rounded-md bg-gray-100 p-4">
+                                <p className="text-sm uppercase">
+                                  {item.bottomBar.title}
+                                </p>
+                                <ul className="mt-2 grid grid-cols-2 gap-x-2 gap-y-1">
+                                  {item.bottomBar.links.map((link) => (
+                                    <li key={link.title}>
+                                      <a href={link.href}>{link.title}</a>
+                                    </li>
+                                  ))}
+                                </ul>
+                              </div>
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </header>
+        <p className="mb-3 text-2xl italic md:hidden">
+          ⚠️This example is only made for desktop.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <p className="max-w-[20ch] text-3xl font-bold leading-[1.1] md:text-[100px]">
+          Don't use this in production yet, unless you know about it's
+          shortcomings. See video.
+        </p>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <p className="mt-2">
+          Of course this mega menu is inspired by Stripe's amazing work
+        </p>
+      </main>
+    </div>
+  );
 }
+
+export default Home;
